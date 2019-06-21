234d01e3f32de47727c27ef08a81835ecf3fe24a
ipl4dc <ipl4dc@inf.elte.hu>
2019-06-21 17:38:34 +0200

    printing img height and width but now with openCV library

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import marvin.image.MarvinImage;
-import marvin.io.MarvinImageIO;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.load("C:/Users/murad/Desktop/New folder (2)/opencv/build/java/x64/opencv_java410.dll");
 
-        MarvinImage image= MarvinImageIO.loadImage("../res/pineapple.jpg");
-        int imgHeight=image.getHeight();
-        int imgWidth=image.getWidth();
-        System.out.println("The image size is: "+imgHeight+" x "+imgWidth);
+        Mat img = Imgcodecs.imread("../res/pineapple.jpg");
+        System.out.println("Image size is: "+img.width()+" x "+img.height());
+
 
 
     }
