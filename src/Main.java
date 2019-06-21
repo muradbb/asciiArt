c23bf8908654fc4e13ef0062a05a3c5431b1303c
ipl4dc <ipl4dc@inf.elte.hu>
2019-06-21 19:40:20 +0200

    inputting the RGBs of every pixel into an array(pixelMatrix) and printing it

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,40 @@ public class Main {
             System.err.println(e.getMessage());
         }
     }
+    static int[] getSepRGB(int p){
+        int red = (p>>16) & 0xff;
+        int green = (p>>8) & 0xff;
+        int blue = p & 0xff;
+        int[] a= {red,blue,green};
+        return a;
+    }
+
 
 
     public static void main(String[] args) {
         loadImage();
-        System.out.println(img.getHeight());
+
+        int[][][]pixelMatrix = new int[img.getHeight()][img.getWidth()][3];
+        for (int i = 0; i <img.getHeight() ; i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                pixelMatrix[i][j]=getSepRGB(img.getRGB(j,i));
+            }
+        }
+        for (int i = 0; i <img.getHeight() ; i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                for (int k = 0; k <3 ; k++) {
+                    if(k==2){
+                        System.out.println(pixelMatrix[i][j][2]);
+                    }else {
+                        System.out.print(pixelMatrix[i][j][k]+" ");
+                    }
+
+                }
+            }
+        }
+
+
+
 
 
 
